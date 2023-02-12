467a32b6acc55fbb2ef62300c23d0170f9778215
Chris Burgess <cgburgess10@gmail.com>
2023-02-11 19:42:52 -0500

    Fix theme

--- src/main.tsx
+++ src/main.tsx
@@ -5,22 +5,19 @@ import { useLocalStorage } from "usehooks-ts";
 import App from "./App";
 import "./index.less";
 
-// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-//   <React.StrictMode>
-//     <CustomProvider theme="light">
-//       <App />
-//     </CustomProvider>
-//   </React.StrictMode>
-// );
-
-// rewrite this so that app has a setTheme and a theme prop that control
-// the theme.
+function add_theme(theme: string) {
+  document.body.classList.remove("rs-theme-light");
+  document.body.classList.remove("rs-theme-high-contrast");
+  document.body.classList.remove("rs-theme-dark");
+  document.body.classList.add(`rs-theme-${theme}`);
+}
 
 function Index() {
   const [theme, setTheme] = useLocalStorage("theme", "dark");
+  add_theme(theme);
   return (
     <React.StrictMode>
-      <CustomProvider theme={theme}>
+      <CustomProvider>
         <App setTheme={setTheme} theme={theme} />
       </CustomProvider>
     </React.StrictMode>
